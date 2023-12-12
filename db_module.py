7c4b678a818f26fac79a4a86657a22dc89fcbc8b
Eric Lin <ericlinsechs@gmail.com>
2023-12-12 14:42:44 +0800

    Update db_module.py

--- db_module.py
+++ db_module.py
@@ -2,17 +2,17 @@ import sqlite3
 
 
 class Database:
-    def __init__(self, db_path):
-        self.connection = sqlite3.connect(db_path, check_same_thread=False)
+    def __init__(self, db_path, check_same_thread=True):
+        self.connection = sqlite3.connect(db_path, check_same_thread=check_same_thread)
         self.cursor = self.connection.cursor()
 
     def execute_query(self, query, *args):
         self.cursor.execute(query, args)
+        self.connection.commit()  # Commit changes to the database
         columns = (
             [column[0] for column in self.cursor.description]
             if self.cursor.description
             else []
         )
         result = [dict(zip(columns, row)) for row in self.cursor.fetchall()]
-        self.connection.commit()  # Commit changes to the database
         return result
