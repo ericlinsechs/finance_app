7d26913edf4f2d2a16fa0d5ca5163ea624682cbb
Eric Lin <ericlinsechs@gmail.com>
2023-12-12 22:27:47 +0800

    Add create_tables() and transactions table

--- db_module.py
+++ db_module.py
@@ -5,6 +5,7 @@ class Database:
     def __init__(self, db_path, check_same_thread=False):
         self.connection = sqlite3.connect(db_path, check_same_thread=check_same_thread)
         self.cursor = self.connection.cursor()
+        self.create_tables()
 
     def execute_query(self, query, *args):
         self.cursor.execute(query, args)
@@ -16,3 +17,34 @@ class Database:
         )
         result = [dict(zip(columns, row)) for row in self.cursor.fetchall()]
         return result
+
+    def create_tables(self):
+        # Create 'users' table if not exists
+        create_users_table_query = """
+            CREATE TABLE IF NOT EXISTS users (
+                id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+                username TEXT NOT NULL,
+                hash TEXT NOT NULL,
+                cash NUMERIC NOT NULL DEFAULT 10000.00
+            );
+        """
+        self.execute_query(create_users_table_query)
+
+        # Create unique index for 'users' table if not exists
+        create_users_index_query = """
+            CREATE UNIQUE INDEX IF NOT EXISTS username ON users (username);
+        """
+        self.execute_query(create_users_index_query)
+
+        # Create 'transactions' table if not exists
+        create_transactions_table_query = """
+            CREATE TABLE IF NOT EXISTS transactions (
+                id INTEGER PRIMARY KEY NOT NULL,
+                user_id INTEGER NOT NULL,
+                symbol TEXT NOT NULL,
+                price NUMERIC NOT NULL,
+                shares NUMERIC NOT NULL,
+                FOREIGN KEY(user_id) REFERENCES users(id)
+            );
+        """
+        self.execute_query(create_transactions_table_query)
--- tests/test_db_module.py
+++ tests/test_db_module.py
@@ -10,24 +10,40 @@ class TestDatabase(unittest.TestCase):
         self.db_fd, self.db_path = tempfile.mkstemp()
         self.db = Database(self.db_path)
 
-        # Initialize the database schema (assuming it's the same as finance.db)
-        schema = """
-            CREATE TABLE users (
-                id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
-                username TEXT NOT NULL,
-                hash TEXT NOT NULL,
-                cash NUMERIC NOT NULL DEFAULT 10000.00
-            );
-            CREATE UNIQUE INDEX username ON users (username);
-        """
-        self.db.cursor.executescript(schema)
-        self.db.connection.commit()
-
     def tearDown(self):
         # Close and remove the temporary database file
         self.db.connection.close()
-        os.close(self.db_fd)
         os.remove(self.db_path)
+
+    def test_create_tables(self):
+        # Call the create_tables method
+        self.db.create_tables()
+
+        # Check if the 'users' table exists
+        query = "SELECT name FROM sqlite_master WHERE type='table' AND name='users';"
+        result_users = self.db.execute_query(query)
+
+        # Check if the 'transactions' table exists
+        query = (
+            "SELECT name FROM sqlite_master WHERE type='table' AND name='transactions';"
+        )
+        result_transactions = self.db.execute_query(query)
+
+        # Check if the 'users' table has the correct columns
+        query = "PRAGMA table_info(users);"
+        columns_users = self.db.execute_query(query)
+
+        # Check if the 'transactions' table has the correct columns
+        query = "PRAGMA table_info(transactions);"
+        columns_transactions = self.db.execute_query(query)
+
+        # Assertions
+        self.assertTrue(result_users)
+        self.assertTrue(result_transactions)
+        self.assertEqual(len(columns_users), 4)  # id, username, hash, cash
+        self.assertEqual(
+            len(columns_transactions), 5
+        )  # id, user_id, symbol, price, shares
 
     def test_execute_query(self):
         # Test a simple query
